9f6609dddb121a5ac9b4596312e5df1efebc60d9
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2025-11-24 16:22:00 +0530

    Create additionsysarg.py

---
+++ additionsysarg.py
@@ -0,0 +1,17 @@
+import sys
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 3:
+        print("Usage: python add.py <num1> <num2>")
+        sys.exit(1)
+
+    
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+
+    
+    print("Sum:", add(x, y))
